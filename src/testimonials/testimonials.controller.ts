bdfa732f20a3ea3975b15b2d9fec16abeaf1d1fb
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-17 16:15:27 +0545

    validation pipe & formDataRequest added in testimonial contact

--- src/testimonials/testimonials.controller.ts
+++ src/testimonials/testimonials.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TestimonialsService } from './testimonials.service';
 import { CreateTestimonialDto } from './dto/create-testimonial.dto';
 import { UpdateTestimonialDto } from './dto/update-testimonial.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { FileSystemStoredFile, FormDataRequest } from 'nestjs-form-data';
 
 @ApiTags('testimonials')
 @Controller('testimonials')
 export class TestimonialsController {
-  constructor(private readonly testimonialsService: TestimonialsService) {}
+  constructor(private readonly testimonialsService: TestimonialsService) { }
 
   @Post()
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  @FormDataRequest({ storage: FileSystemStoredFile })
   create(@Body() createTestimonialDto: CreateTestimonialDto) {
     return this.testimonialsService.create(createTestimonialDto);
   }
@@ -25,6 +28,8 @@ export class TestimonialsController {
   }
 
   @Patch(':id')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  @FormDataRequest({ storage: FileSystemStoredFile })
   update(@Param('id') id: string, @Body() updateTestimonialDto: UpdateTestimonialDto) {
     return this.testimonialsService.update(id, updateTestimonialDto);
   }
