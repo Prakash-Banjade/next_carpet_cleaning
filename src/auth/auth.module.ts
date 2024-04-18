2a83bce479c934c34844db7f1fb386cf41aabbe3
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-18 13:00:06 +0545

    removed unused import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
 import { AuthGuard } from './auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { MembersModule } from 'src/members/members.module';
