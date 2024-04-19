625d40ce7b2c2cd7f1b6665cdb296c1a22e5760d
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-19 12:20:58 +0545

    updated dto

--- src/members/dto/update-member.dto.ts
+++ src/members/dto/update-member.dto.ts
@@ -2,8 +2,9 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateMemberDto } from './create-member.dto';
 import { FileSystemStoredFile } from 'nestjs-form-data';
 import { IsOptional } from 'class-validator';
+import { OmitType } from '@nestjs/mapped-types';
 
-export class UpdateMemberDto extends PartialType(CreateMemberDto) {
+export class UpdateMemberDto extends OmitType(CreateMemberDto, ['image']) {
     @IsOptional()
     image?: string | FileSystemStoredFile;
 }
