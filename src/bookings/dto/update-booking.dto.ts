40e3c3c4dbf8c615a0cdec4e11298e22648b28a1
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-25 15:06:03 +0545

    Update updateBookingDto

--- src/bookings/dto/update-booking.dto.ts
+++ src/bookings/dto/update-booking.dto.ts
@@ -1,11 +1,44 @@
-import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
-import { CreateBookingDto } from './create-booking.dto';
-import { OmitType } from '@nestjs/mapped-types';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Status } from '../entities/booking.entity';
-import { IsEnum } from 'class-validator';
+import { IsEmail, IsEnum, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
 
-export class UpdateBookingDto extends PartialType(OmitType(CreateBookingDto, ['name', 'phone'])) {
+export class UpdateBookingDto {
     @ApiPropertyOptional({ type: 'enum', enum: Status, description: 'status of booking' })
     @IsEnum(Status, { message: 'status must be pending, successed or rejected' })
-    status: Status
+    status?: Status
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    name?: string;
+
+    @ApiPropertyOptional()
+    @IsEmail()
+    @IsOptional()
+    email?: string;
+
+    @ApiPropertyOptional()
+    @IsPhoneNumber('AU', { message: 'Invalid phone number. Must be Australian.' })
+    @IsOptional()
+    phone?: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    location?: string;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    service?: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    message?: string;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    time?: string;
 }
