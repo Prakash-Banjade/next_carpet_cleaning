1b5da0c19f5fe62d61d6c0f86f65bc34c3917b38
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-01 13:16:10 +0545

    add meme types

--- src/members/dto/create-member.dto.ts
+++ src/members/dto/create-member.dto.ts
@@ -1,54 +1,65 @@
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, IsUrl } from "class-validator";
-import { FileSystemStoredFile, HasMimeType, IsFile, MemoryStoredFile } from "nestjs-form-data";
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+import { HasMimeType, IsFile, MemoryStoredFile } from 'nestjs-form-data';
 
 export class CreateMemberDto {
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    name: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-    @ApiProperty()
-    @IsEmail()
-    @IsString()
-    email: string;
+  @ApiProperty()
+  @IsEmail()
+  @IsString()
+  email: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    post: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  post: string;
 
-    @ApiProperty({ type: 'string', format: 'binary' })
-    @HasMimeType(['image/jpeg', 'image/png', 'image/webp'], { message: 'Invalid type for image. Image must be jpeg or png or webp' })
-    @IsFile({ message: 'Invalid type for Image. Image must be file type' })
-    image?: MemoryStoredFile;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  @HasMimeType(
+    ['image/jpeg', 'image/jpg', 'image/png', 'image/png', 'image/webp'],
+    { message: 'Invalid type for image. Image must be jpeg or png or webp' },
+  )
+  @IsFile({ message: 'Invalid type for Image. Image must be file type' })
+  image?: MemoryStoredFile;
 
-    @ApiPropertyOptional()
-    @IsString()
-    @IsUrl()
-    @IsOptional()
-    facebook?: string;
+  @ApiPropertyOptional()
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  facebook?: string;
 
-    @ApiPropertyOptional()
-    @IsString()
-    @IsUrl()
-    @IsOptional()
-    instagram?: string;
+  @ApiPropertyOptional()
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  instagram?: string;
 
-    @ApiPropertyOptional()
-    @IsString()
-    @IsUrl()
-    @IsOptional()
-    twitter?: string;
+  @ApiPropertyOptional()
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  twitter?: string;
 
-    @IsString()
-    @ApiPropertyOptional()
-    @IsUrl()
-    @IsOptional()
-    linkedIn?: string;
+  @IsString()
+  @ApiPropertyOptional()
+  @IsUrl()
+  @IsOptional()
+  linkedIn?: string;
 
-    @ApiProperty() 
-    @IsPhoneNumber('AU', { message: 'Invalid phone number. Must be an Australian phone number' })
-    phone?: string;
+  @ApiProperty()
+  @IsPhoneNumber('AU', {
+    message: 'Invalid phone number. Must be an Australian phone number',
+  })
+  phone?: string;
 }
-
