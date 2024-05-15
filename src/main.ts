2f270fb40971cab35d84fda61488d31245c008b9
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-15 17:13:42 +0545

    no logs in app

--- src/main.ts
+++ src/main.ts
@@ -23,9 +23,7 @@ import { ValidationPipe } from '@nestjs/common';
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['log'],
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
